27ad671da3ad6107f54d5b4c6b4c96b6ab0e1f7e
anasvakyathodi <anasvakyathodi@gmail.com>
2020-10-06 09:36:52 +0530

    env variable update

---
+++ blog-backend/config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MONGODB_URI: process.env.MONGODB_URI,
+};
--- blog-backend/server.js
+++ blog-backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const config = require("./config/key");
 require("dotenv").config();
 
 // setup express
@@ -19,7 +20,7 @@ app.use("/posts", require("./routes/postRoutes"));
 // setup mongoose
 
 mongoose.connect(
-  process.env.MONGODB_URI,
+  config.MONGODB_URI,
   { useNewUrlParser: true, useUnifiedTopology: true },
   (err) => {
     if (err) {
@@ -28,5 +29,12 @@ mongoose.connect(
     console.log("MongoDB connection success !!!");
   }
 );
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "blog", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "blog", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
