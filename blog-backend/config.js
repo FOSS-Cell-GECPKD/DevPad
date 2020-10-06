43d700c2119e6b2f4878ef7edeb15df4ea556043
ayush-git228 <66081505+ayush-git228@users.noreply.github.com>
2020-10-06 20:22:32 +0530

    Add files via upload

---
+++ blog-backend/config.js
@@ -0,0 +1,4 @@
+//This config file use: Store all configuration information about the server. A way of centralizing all the configuration for our server.
+module.exports={
+    "secretKey":"12345-67890-09846-54123"   //secret key to sign in our json web token.
+}
