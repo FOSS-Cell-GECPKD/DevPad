c418963098a6911b64fce7c63f0d4edacb44566c
Gia Yan Yan Chan <giayanny926@gmail.com>
2020-10-07 14:03:40 +1100

    changed npm run dev script to fix concurrent issue & chnaged Light theme better color combination

--- blog/src/Theme/Theme.js
+++ blog/src/Theme/Theme.js
@@ -1,37 +1,49 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
 export const light_theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#202020",
-      dark: "#fff",
-      background: {
-        default: "#fff",
-        paper: "#121212",
-      },
-    },
-  },
+	palette: {
+		primary: { main: "#000" },
+		secondary: {
+			main: "#020f17"
+		},
+		background: {
+			default: "#f1f7fb",
+			paper: "#A3DCFF"
+		},
+		text: {
+			primary: "#000",
+			secondary: "#020f17",
+			disabled: "#8BBBD9"
+		},
+		action: {
+			disabled: "#8BBBD9"
+		},
+		info: {
+			main: "#8b94d9"
+		}
+	}
 });
+
 export const dark_theme = createMuiTheme({
-  palette: {
-    primary: { main: "#fff" },
-    secondary: {
-      main: "#bbe1fa",
-    },
-    background: {
-      default: "#1b262c",
-      paper: "#0f4c75",
-    },
-    text: {
-      primary: "#fff",
-      secondary: "#bbe1fa",
-      disabled: "#0f4c75",
-    },
-    action: {
-      disabled: "#0f4c75",
-    },
-    info: {
-      main: "#0f4c75",
-    },
-  },
+	palette: {
+		primary: { main: "#fff" },
+		secondary: {
+			main: "#bbe1fa"
+		},
+		background: {
+			default: "#1b262c",
+			paper: "#0f4c75"
+		},
+		text: {
+			primary: "#fff",
+			secondary: "#bbe1fa",
+			disabled: "#0f4c75"
+		},
+		action: {
+			disabled: "#0f4c75"
+		},
+		info: {
+			main: "#0f4c75"
+		}
+	}
 });
