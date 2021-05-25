e8b42f31225718e01c227342fd17b35d952f9679
afif ahmed <afifahmed456123@gmail.com>
2021-05-25 10:02:00 +0530

    updated post and user model

--- blog-backend/config.js
+++ blog-backend/config.js
@@ -1,4 +1,5 @@
 //This config file use: Store all configuration information about the server. A way of centralizing all the configuration for our server.
-module.exports={
-    "secretKey":"12345-67890-09846-54123"   //secret key to sign in our json web token.
-}
+module.exports = {
+	secretKey: '12345-67890-09846-54123',
+	MONGODB_URI: 'mongodb://localhost:27017/devpad', //secret key to sign in our json web token.
+};
--- blog-backend/server.js
+++ blog-backend/server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const config = require("./config/key");
-const passport = require("passport");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./config/key');
+const passport = require('passport');
 //require("dotenv").config();
 
 // setup express
@@ -13,30 +13,30 @@ const PORT = process.env.PORT || 5000;
 // setup mongoose
 
 mongoose.connect(
-  config.MONGODB_URI,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log("MongoDB connection success !!!");
-  }
+	config.MONGODB_URI,
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+	(err) => {
+		if (err) {
+			console.log(err);
+		}
+		console.log('MongoDB connection success !!!');
+	}
 );
 
 app.use(express.json());
-app.use(passport.initialize());   // Should be used before express.Router always
+app.use(passport.initialize()); // Should be used before express.Router always
 // setup routes
-express.Router().get("/hello", (req, res) => {
-  res.send("Hello world");
+express.Router().get('/hello', (req, res) => {
+	res.send('Hello world');
 });
-app.use("/posts", require("./routes/postRoutes"));
-app.use("/users", require("./routes/users"));
+app.use('/posts', require('./routes/postRoutes'));
+app.use('/users', require('./routes/users'));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "blog", "build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "blog", "build", "index.html"));
-  });
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, 'blog', 'build')));
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'blog', 'build', 'index.html'));
+	});
 }
 
 app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
